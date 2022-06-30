75d0a902b350ce304ad575dcd872d373c2d248bf
moeibrahim28 <m.ibrahim0228@gmail.com>
2022-06-30 14:32:44 -0400

    garage and car classes created

---
+++ src/main/java/Garage.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Garage {
+   private List<Car> garage;
+
+
+  public Garage(){
+    this.garage = new ArrayList<>();
+    garage.add(new Car("Toyota", "4Runner"));
+    garage.add(new Car("BMW", "M4"));
+    garage.add(new Car("Mercedes Benz", "C300"));
+    garage.add(new Car("Audi", "A6"));
+   }
+
+   public void addCar(Car car){
+    garage.add(car);
+   }
+
+public List<Car> getGarage() {
+    return garage;
+}
+
+public void setGarage(List<Car> garage) {
+    this.garage = garage;
+}
+
+@Override
+public String toString() {
+    return "Garage [garage=" + garage + "]";
+}
+
+   
+}
---
+++ src/main/java/Runner.java
@@ -0,0 +1,6 @@
+public class Runner {
+    public static void main(String[] args) {
+        Garage garage=new Garage();
+        System.out.println(garage);
+    }
+}
