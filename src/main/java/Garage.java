697fc54df027bcdfeee61409e6be055149d26d8d
moeibrahim28 <m.ibrahim0228@gmail.com>
2022-06-30 15:07:30 -0400

    formatted and fixed all requested methods

--- src/main/java/Garage.java
+++ src/main/java/Garage.java
@@ -2,37 +2,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Garage {
-   private List<Car> garage;
+    private List<Car> garage;
 
+    public Garage() {
+        this.garage = new ArrayList<>();
+        garage.add(new Car("Toyota", "4Runner"));
+        garage.add(new Car("BMW", "M4"));
+        garage.add(new Car("Mercedes Benz", "C300"));
+        garage.add(new Car("Audi", "A6"));
+    }
 
-  public Garage(){
-    this.garage = new ArrayList<>();
-    garage.add(new Car("Toyota", "4Runner"));
-    garage.add(new Car("BMW", "M4"));
-    garage.add(new Car("Mercedes Benz", "C300"));
-    garage.add(new Car("Audi", "A6"));
-   }
+    public void addCar(Car car) {
+        garage.add(car);
+    }
 
-   public void addCar(Car car){
-    garage.add(car);
-   }
+    public void set(int index, Car car) {
+        garage.set(index, car);
+    }
 
-public List<Car> getGarage() {
-    return garage;
-}
+    public Car get(int index) {
+        return garage.get(index);
+    }
 
-public void set(int index, Car car){
-    garage.set(index, car);
-}
+    public List<Car> getGarage() {
+        return garage;
+    }
 
-public void setGarage(List<Car> garage) {
-    this.garage = garage;
-}
+    public void setGarage(List<Car> garage) {
+        this.garage = garage;
+    }
 
-@Override
-public String toString() {
-    return "Garage [garage=" + garage + "]";
-}
+    @Override
+    public String toString() {
+        return "Garage [garage=" + garage + "]";
+    }
 
-   
 }
